a514fa496b9da6fb7047a413eef6dc8b25cbd4a4
medicoforever001996 <91033346+medicoforever@users.noreply.github.com>
2025-09-08 23:23:38 +0530

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -8,14 +8,24 @@
 // 8. FIND CLIENT ID: Go to Project Settings -> General. Scroll down to find your SDK setup and configuration. Your Web client ID for Google Sign-In is listed here. Copy it to `googleClientId` below.
 
 export const firebaseConfig = {
-  // PASTE YOUR FIREBASE CONFIG OBJECT HERE
-  apiKey: "AIzaXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:1234567890abcdef",
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsvaphTIz261oTCBLE9pd7sc6q4WxavCs",
+  authDomain: "lkmkbgndkb.firebaseapp.com",
+  projectId: "lkmkbgndkb",
+  storageBucket: "lkmkbgndkb.firebasestorage.app",
+  messagingSenderId: "1087976758964",
+  appId: "1:1087976758964:web:fa742ebbd4063bdd3f41e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
   // PASTE YOUR GOOGLE WEB CLIENT ID FOR GOOGLE SIGN-IN HERE
-  googleClientId: "123456789012-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com",
+  googleClientId: "1087976758964-0dfeh4n99t0io2m4efsbhkrllu68s7lb.apps.googleusercontent.com",
 };
